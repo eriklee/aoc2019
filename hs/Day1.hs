c3615a63acf306b4ac52ca082bca1d36bc9feaf2
Erik Lee <eriklee113@gmail.com>
2019-12-01 11:59:01 +0000

    Day1: Some lovely haskell
    stack build from the root directory should work...

---
+++ hs/Day1.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE TupleSections #-}
+module Day1 where
+import Data.List (unfoldr)
+import Control.Monad (join)
+
+main :: IO ()
+main = do
+  putStr "Part 1: "
+  print =<< day1_1 "input/day1.txt"
+  putStr "Part 2: "
+  print =<< day1_2 "input/day1.txt"
+
+day1_1 :: String -> IO Int
+day1_1 fn = sum . map (fuelRequired . read) . lines <$> readFile fn
+
+fuelRequired :: Int -> Int
+fuelRequired = (+ (-2)) . floor . (/3) . fromIntegral
+
+day1_2 :: String -> IO Int
+day1_2 fn = sum . join . map (allFuel . read) . lines <$> readFile fn
+
+isPos :: Int -> Maybe Int
+isPos x = if x < 0 then Nothing else Just x
+
+allFuel :: Int -> [Int]
+allFuel = unfoldr (\x -> dupe <$> (isPos . fuelRequired) x)
+
+dupe :: a -> (a,a)
+dupe = join (,)
---
+++ hs/Main.hs
@@ -0,0 +1,6 @@
+module Main where
+
+import qualified Day1 as D1 (main)
+main :: IO ()
+main = do
+  D1.main
